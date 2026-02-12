460d0d5b7b8d73f198c99234ed5a9cc7ff5cf72e
Kapten boneng <iwak01bogo@gmail.com>
2026-02-12 11:53:38 +0700

    Create signer.ts

---
+++ security/signer.ts
@@ -0,0 +1,12 @@
+import crypto from "crypto";
+
+export function verifySignature(
+  message: string,
+  signature: string,
+  publicKey: string
+): boolean {
+  const verify = crypto.createVerify("SHA256");
+  verify.update(message);
+  verify.end();
+  return verify.verify(publicKey, signature, "hex");
+}
